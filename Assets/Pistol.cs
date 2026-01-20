46fc6ee62f3d28def7a31b7ac98dd4433b7fa109
iKripto <yagosiqueira450@gmail.com>
2026-01-20 10:38:02 -0300

    Grab, armas funcionais, interação, sockets e mais muitas outras funções aprendidas.

---
+++ Assets/Pistol.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [Header("Bullet Settings")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 20f;
+    public float bulletLifetime = 5f;
+
+    public AudioClip clip;
+    private AudioSource source;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        source.PlayOneShot(clip);
+
+        if (rb != null)
+        {
+            rb.linearVelocity = firePoint.forward * bulletSpeed;
+        }
+        Destroy(bullet, bulletLifetime);
+    }
+}
